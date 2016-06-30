ee4129a3b9feab489957f16604adc4cb6b49bb66
chok <chokm58@gmail.com>
2016-06-30 11:03:34 +0000

    Add correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -37,5 +38,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation,
                                     :area, :profile)
-    end   
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      if current_user != @user
+        redirect_to root_path
+      end
+    end
 end
